ed0e0f603419698c939e76acbd3a5cccde65cc06
陈昊天 <2363822647@qq.com>
2022-02-09 17:22:01 +0800

    train 2.9

---
+++ Algorithms and Data Structures/Math/P1082_The_same_residual_equation.cpp
@@ -0,0 +1,33 @@
+//#pragma GCC optimize(2)
+//std::ios::sync_with_stdio(0)
+//clock_t st=clock();
+#include<bits/stdc++.h>
+#define abss(x) ((x)>(0)?(x):(-1)*(x))
+#define maxs(a,b) ((a)>(b)?(a):(b))
+#define mins(a,b) ((a)<(b)?(a):(b))
+#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
+#define ROF(i,a,b) for(int i=(a);i>=(b);--i)
+#define mem(a) memset(a,0,sizeof(a))
+const int INF (1<<30);
+const int inf (-1<<30);
+using namespace std;
+
+void exgcd(int &x,int &y,int a,int b){
+    if(!b){
+        x=1,y=0;
+        return;
+    }
+    exgcd(x,y,b,a%b);
+    int t;
+    t=x,x=y,y=t-a/b*y;
+}
+
+int main(){
+	int x,y;
+	cin>>x>>y;
+    const int Y=y;
+	exgcd(x,y,x,y);
+	x=(x%Y+Y)%Y;//负数回正，大的变小，得到最小正整数解
+    cout<<x;
+	return 0;
+}
