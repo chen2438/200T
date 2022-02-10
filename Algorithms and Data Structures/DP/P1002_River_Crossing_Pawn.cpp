270d41673fbe2e2ae08fe4cc57597d08fe5f5600
陈昊天 <2363822647@qq.com>
2022-02-10 21:09:22 +0800

    Update P1002_River_Crossing_Pawn.cpp

--- Algorithms and Data Structures/DP/P1002_River_Crossing_Pawn.cpp
+++ Algorithms and Data Structures/DP/P1002_River_Crossing_Pawn.cpp
@@ -1,31 +1,28 @@
-#include<iostream>
-#include<cstring>
-#include<cstdio>
-#include<algorithm>
+#include<bits/stdc++.h>
+#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
 #define ll long long
 using namespace std;
 
-const int fx[] = {0, -2, -1, 1, 2, 2, 1, -1, -2};
-const int fy[] = {0, 1, 2, 2, 1, -1, -2, -2, -1};
-//马可以走到的位置
+int dx[]={ 0,-2,-1, 1, 2, 2, 1,-1,-2};
+int dy[]={ 0, 1, 2, 2, 1,-1,-2,-2,-1};
 
-int bx, by, mx, my;
-ll f[40][40];
-bool s[40][40]; //判断这个点有没有马拦住
+int bx,by,mx,my;
+ll dp[40][40];
+bool mark[40][40];
+
 int main(){
     cin>>bx>>by>>mx>>my;
-    bx += 2; by += 2; mx += 2; my += 2;
-    //坐标+2以防越界
-    f[2][1] = 1;//初始化
-    s[mx][my] = 1;//标记马的位置
-    for(int i = 1; i <= 8; i++) s[mx + fx[i]][my + fy[i]] = 1;
-    for(int i = 2; i <= bx; i++){
-        for(int j = 2; j <= by; j++){
-            if(s[i][j]) continue; // 如果被马拦住就直接跳过
-            f[i][j] = f[i - 1][j] + f[i][j - 1];
-            //状态转移方程
+    bx+=2,by+=2,mx+=2,my+=2;
+    mark[mx][my]=true;
+    FOR(i,1,8)
+        mark[mx+dx[i]][my+dy[i]]=true;
+    dp[2][1]=1;
+    FOR(i,2,bx){
+        FOR(j,2,by){
+            if(mark[i][j]) continue;
+            dp[i][j]=dp[i-1][j]+dp[i][j-1];
         }
     }
-    printf("%lld\n", f[bx][by]);
+    cout<<dp[bx][by];
     return 0;
 } 
