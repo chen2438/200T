a147597ad2f11434c183f7a4c872c8bc8b69eb21
陈昊天 <53367039+chen2438@users.noreply.github.com>
2022-02-11 16:21:18 +0800

    tran 2.11

--- Algorithms and Data Structures/DP/P1439_LCS.cpp
+++ Algorithms and Data Structures/DP/P1439_LCS.cpp
@@ -11,24 +11,30 @@ const int INF (1<<30);
 const int inf (-1<<30);
 using namespace std;
 
-const int maxn=100007;
-int a[maxn],b[maxn];
-int dp[maxn][maxn];
+const int maxn=1e5+7;
+int a1[maxn],a2[maxn],dp1[maxn],dp2[maxn];
 
-int main(){
-    int n;
-    cin>>n;
-    FOR(i,1,n){
-        cin>>a[i];
-    }
-    FOR(i,1,n){
-        cin>>b[i];
-    }
-    FOR(i,1,n)
-        FOR(j,1,n){
-            if(a[i]==b[j]) dp[i][j]=dp[i-1][j-1]+1;
-            else dp[i][j]=maxs(dp[i][j-1],dp[i-1][j]);
-        }
-    cout<<dp[n][n]<<endl;
-    return 0;
+int main() {
+	int n;cin>>n;
+	while(cin>>a[n]) ++n;
+	n--;
+
+	int len1=1,len2=1;
+	dp1[1]=a[1];
+	dp2[1]=a[1];
+
+	FOR(i,2,n){
+		if(dp1[len1]>=a[i]) dp1[++len1]=a[i];
+		else{
+			int p1=upper_bound(dp1+1,dp1+1+len1,a[i],greater<int>())-dp1;
+			dp1[p1]=a[i]; 
+		}
+		if(dp2[len2]<a[i]) dp2[++len2]=a[i];
+		else{
+			int p2=lower_bound(dp2+1,dp2+1+len2,a[i],less<int>())-dp2;
+			dp2[p2]=a[i];
+		}
+	}
+	cout<<len1<<endl<<len2;
+	return 0;
 }
