e2da041fdd7153269b568dc11635bafde65d8ab0
陈昊天 <2363822647@qq.com>
2022-02-11 17:04:12 +0800

    train 2.11

--- Algorithms and Data Structures/DP/P1439_LCS.cpp
+++ Algorithms and Data Structures/DP/P1439_LCS.cpp
@@ -1,40 +1,34 @@
-//#pragma GCC optimize(2)
-//clock_t st=clock();
 #include<bits/stdc++.h>
-#define abss(x) ((x)>(0)?(x):(-1)*(x))
-#define maxs(a,b) ((a)>(b)?(a):(b))
-#define mins(a,b) ((a)<(b)?(a):(b))
 #define FOR(i,a,b) for(int i=(a);i<=(b);++i)
-#define ROF(i,a,b) for(int i=(a);i>=(b);--i)
 #define mem(a) memset(a,0,sizeof(a))
-const int INF (1<<30);
-const int inf (-1<<30);
 using namespace std;
 
+#define map _map
 const int maxn=1e5+7;
-int a1[maxn],a2[maxn],dp1[maxn],dp2[maxn];
+int a2[maxn],dp[maxn],map[maxn];
 
 int main() {
 	int n;cin>>n;
-	while(cin>>a[n]) ++n;
-	n--;
+	int x;
+	FOR(i,1,n){
+		cin>>x;//x=a1[i]
+		map[x]=i;//a1[i] -> i
+	}
+	FOR(i,1,n){
+		cin>>x;
+		a2[i]=map[x];
+	}
 
-	int len1=1,len2=1;
-	dp1[1]=a[1];
-	dp2[1]=a[1];
+	int len=1;
+	dp[1]=a2[1];
 
 	FOR(i,2,n){
-		if(dp1[len1]>=a[i]) dp1[++len1]=a[i];
+		if(dp[len]<=a2[i]) dp[++len]=a2[i];
 		else{
-			int p1=upper_bound(dp1+1,dp1+1+len1,a[i],greater<int>())-dp1;
-			dp1[p1]=a[i]; 
-		}
-		if(dp2[len2]<a[i]) dp2[++len2]=a[i];
-		else{
-			int p2=lower_bound(dp2+1,dp2+1+len2,a[i],less<int>())-dp2;
-			dp2[p2]=a[i];
+			int p1=upper_bound(dp+1,dp+1+len,a2[i],less<int>())-dp;
+			dp[p1]=a2[i]; 
 		}
 	}
-	cout<<len1<<endl<<len2;
+	cout<<len;
 	return 0;
 }
