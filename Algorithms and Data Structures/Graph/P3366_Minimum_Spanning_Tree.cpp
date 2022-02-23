426615287f943f6909851103e038bab8043af08c
陈昊天 <2363822647@qq.com>
2022-02-23 18:39:22 +0800

    train 2.23

--- Algorithms and Data Structures/Graph/P3366_Minimum_Spanning_Tree.cpp
+++ Algorithms and Data Structures/Graph/P3366_Minimum_Spanning_Tree.cpp
@@ -2,59 +2,55 @@
 #define FOR(i,a,b) for(int i=(a);i<=(b);++i)
 using namespace std;
 
-#define inf (1<<30)
-#define maxn 5005
-#define maxm 200005
-struct edge{
-	int v,w,next;
-}e[maxm<<1];//无向图开两倍数组
-int n,m;
-int dis[maxn],now=1,ans;
-//dis[]=已经加入最小生成树的的点到没加入点的最短距离
-//比如说1和2号节点已经加入了最小生成树，那么dis[3]就等于min(1->3,2->3)
-bool vis[maxn];
+struct Edge{
+    int u,v,w;
+}edge[200005];
 
-int head[maxn],cnt;
-void add(int u,int v,int w){
-	e[++cnt].v=v;
-	e[cnt].w=w;
-	e[cnt].next=head[u];
-	head[u]=cnt;
+int fa[5005],n,m;
+
+bool cmp(Edge a,Edge b){
+    return a.w<b.w;
 }
 
-int prim(){
-	FOR(i,2,n)
-		dis[i]=inf;
-	for(int i=head[1];i;i=e[i].next)
-		dis[e[i].v]=min(dis[e[i].v],e[i].w);
-    FOR(j,1,n-1){
-        int minn=inf;
-        vis[now]=1;
-        FOR(i,1,n){
-            if(!vis[i] and minn>dis[i]){
-                minn=dis[i];
-				now=i;
-            }
-        }
-        if(minn==inf) return 0;
-        ans+=minn;
-        for(int i=head[now];i;i=e[i].next){
-        	int v=e[i].v;
-        	if(!vis[v] and dis[v]>e[i].w) dis[v]=e[i].w;
-		}
+int find(int x){
+    while(x!=fa[x])
+        x=fa[x]=fa[fa[x]];
+    return x;
+}
+
+int kruskal(){
+    int eu,ev,cnt=0,ans=0;
+    sort(edge,edge+m,cmp);
+    FOR(i,0,m-1){
+        eu=find(edge[i].u),ev=find(edge[i].v);
+        if(eu==ev) continue;
+        ans+=edge[i].w;
+        fa[ev]=eu;
+        if(++cnt==n-1) break;
     }
     return ans;
 }
 
+bool is_connected(){
+    int cnt=0;
+    FOR(i,1,n){
+        if(fa[i]==i) cnt++;
+    }
+    if(cnt==1) return true;
+    else return false;
+}
+
 int main(){
     cin>>n>>m;
-    FOR(i,1,m){
+    FOR(i,1,n)
+        fa[i]=i;
+    FOR(i,0,m-1){
         int u,v,w;
         cin>>u>>v>>w;
-        add(u,v,w),add(v,u,w);
+        edge[i]={u,v,w};
     }
-    int res=prim();
-    if(res) cout<<res;
+    int res=kruskal();
+    if(is_connected()) cout<<res;
     else cout<<"orz";
     return 0;
 }
