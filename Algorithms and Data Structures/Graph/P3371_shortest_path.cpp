e674661a1a46390fc09e3e5e173c5a8a0e845f04
陈昊天 <2363822647@qq.com>
2022-02-24 23:40:51 +0800

    train 2.24

--- Algorithms and Data Structures/Graph/P3371_shortest_path.cpp
+++ Algorithms and Data Structures/Graph/P3371_shortest_path.cpp
@@ -1,16 +1,17 @@
 #include<bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<=(b);++i)
-const int INF=(1<<30);
+#define memmax(a) memset(a,63,sizeof(a))
+const int mmax=1061109567;
 using namespace std;
 
-const int maxn=1e4+7;
+#define maxn 10007
 int n,m,s;//点、边、出发点
 int f[maxn][maxn];
 
 void floyd(){
 	FOR(k,1,n){
 	  	FOR(i,1,n){
-	  		if(i==k or f[i][k]==INF) continue;
+	  		if(i==k or f[i][k]==mmax) continue;
 	    	FOR(j,1,n){
 	      		f[i][j]=min(f[i][j],f[i][k]+f[k][j]);
 	    	}
@@ -20,14 +21,12 @@ void floyd(){
 }
 
 int main(){
+	memmax(f);
 	cin>>n>>m>>s;
-	FOR(i,1,n)
-		FOR(j,1,n)
-			f[i][j]=INF;
 	int u,v,w;
 	FOR(i,1,m){
 		cin>>u>>v>>w;
-		f[u][v]=min(f[u][v],w);//去重边
+		f[u][v]=min(f[u][v],w);
 	}
 	floyd();
 	FOR(i,1,n){
