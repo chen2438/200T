1b8f642ea665e2d4fdaca8f2058efb29d231c0b7
陈昊天 <2363822647@qq.com>
2022-02-25 22:05:10 +0800

    train 2.25

--- Algorithms and Data Structures/Graph/P3371_shortest_path.cpp
+++ Algorithms and Data Structures/Graph/P3371_shortest_path.cpp
@@ -1,36 +1,56 @@
 #include<bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<=(b);++i)
-#define memmax(a) memset(a,63,sizeof(a))
-const int mmax=1061109567;
 using namespace std;
 
-#define maxn 10007
-int n,m,s;//点、边、出发点
-int f[maxn][maxn];
+const int maxn=1e4+7;
+const int maxm=5e5+7;
+const int INF=(1<<30);
 
-void floyd(){
-	FOR(k,1,n){
-	  	FOR(i,1,n){
-	  		if(i==k or f[i][k]==mmax) continue;
-	    	FOR(j,1,n){
-	      		f[i][j]=min(f[i][j],f[i][k]+f[k][j]);
-	    	}
-	  	}
-	}
-	f[s][s]=0;
+struct Edge{
+    int u,v,w;
+};
+
+Edge edge[maxm];
+int dist[maxn];//结点到源点最小距离
+int n,m,s;//结点数,边数,源点
+
+// 初始化图
+void init(){
+    FOR(i,1,n)
+        dist[i]=INF;
+    FOR(i,1,m){
+    	int u,v,w;
+    	cin>>u>>v>>w;
+        edge[i]={u,v,w};
+        if(u==s) dist[v]=w;
+    }
+    dist[s]=0;
 }
 
+void relax(int u,int v,int w){
+    if(dist[v]>dist[u]+w) dist[v]=dist[u]+w;
+}
+
+bool Bellman_Ford(){
+    FOR(i,1,n-1)
+        FOR(j,1,m)
+            relax(edge[j].u,edge[j].v,edge[j].w);
+    bool flag=true;
+    FOR(i,1,m)//判断是否有负环路
+        if(dist[edge[i].v]>dist[edge[i].u]+edge[i].w){
+            flag=false;
+            break;
+        }
+    return flag;
+}
 int main(){
-	memmax(f);
 	cin>>n>>m>>s;
-	int u,v,w;
-	FOR(i,1,m){
-		cin>>u>>v>>w;
-		f[u][v]=min(f[u][v],w);
-	}
-	floyd();
-	FOR(i,1,n){
-		cout<<f[s][i]<<" ";
-	}
-	return 0;
+    init();
+    if(Bellman_Ford()){
+        FOR(i,1,n){
+            if(dist[i]!=INF) cout<<dist[i]<<" ";
+            else cout<<INT_MAX<<" ";
+        }
+    }
+    return 0;
 }
