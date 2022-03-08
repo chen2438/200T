fbf5ef69f33f3f968f3179b312ac003ecaeb66d1
陈昊天 <2363822647@qq.com>
2022-03-08 22:18:19 +0800

    tmp

--- library/tmp.cpp
+++ library/tmp.cpp
@@ -1,25 +1,46 @@
 #include<bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<=(b);++i)
-#define mem(a) memset(a,0,sizeof(a))
-#define maxs(a,b) ((a)>(b)?(a):(b))
-#define scan(a) scanf("%d",&(a))
-#define abss(x) ((x)>(0)?(x):(-1)*(x))
+#define int long long
 using namespace std;
 
-const int maxn=1e3+7;
-const int maxm=1e5+7;
+typedef pair<int,int> PII;
+const int N = 1e5+10;
+vector<int> x[N];
+vector<int> y[N];
 
-set<int> s;
+int n,m;
 
-struct node{
-    int x,y;
-};
-
-vector<node> v[maxm];
-
-
-int main(){
-    v[0].push_back({1,2});
-    cout<<v[0].x<<" "<<v[0].y<<'\n';
-    return 0;
+void solve(){
+    cin>>n>>m;
+    vector c(n+1,vector<int> (m+1));
+    FOR(i,1,n){
+        FOR(j,1,m){
+            int c;cin>>c;
+            x[c].push_back(i);
+            y[c].push_back(j);
+        }
+    }
+    int res=0;
+    FOR(i,1,1e5){
+        int cnt=1,sum=0;
+        sort(x[i].begin(),x[i].end());
+        FOR(j,1,x[i].size()-1){
+            res+=(x[i][j]-x[i][j-1])*cnt+sum;
+            sum+=(x[i][j]-x[i][j-1])*cnt;
+            cnt++;
+        }
+        cnt = 1,sum = 0;
+        sort(y[i].begin(),y[i].end());
+        for(int j = 1;j < y[i].size(); j++){
+            res += (y[i][j] - y[i][j-1]) * cnt + sum;
+            sum += (y[i][j] - y[i][j-1]) * cnt;
+            cnt++;
+        }
+    }
+    cout<<res<<"\n";
+}
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
 }
