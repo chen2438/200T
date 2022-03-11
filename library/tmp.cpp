b62206b1f0088c69c71775c6bfde51093e016137
陈昊天 <2363822647@qq.com>
2022-03-11 10:59:45 +0800

    train 3.11

--- library/tmp.cpp
+++ library/tmp.cpp
@@ -1,23 +1,28 @@
 #include<bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<=(b);++i)
 using namespace std;
-
-int main(){
-    int t,n;
-    
-    while(cin>>t){
-        while(t--){ 
-            int flag=0;
-            cin>>n;
-            FOR(i,1,10000){
-                if((int)sqrt(i*i*n+1)*(int)sqrt(i*i*n+1)==n*i*i+1){
-                    cout<<1<<'\n';
-                    flag=1;
-                    break;
+/*
+    int splitArray(vector<int>& nums, int m) {
+        int n = nums.size();
+        vector<vector<long long>> f(n + 1, vector<long long>(m + 1, LLONG_MAX));
+        //初始化二维数组f[][]，预先申请f[n+1][m+1]空间
+        vector<long long> sub(n + 1, 0);
+        for (int i = 0; i < n; i++) {
+            sub[i + 1] = sub[i] + nums[i];
+        }
+        f[0][0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= min(i, m); j++) {
+                for (int k = 0; k < i; k++) {
+                    f[i][j] = min(f[i][j], max(f[k][j - 1], sub[i] - sub[k]));
                 }
             }
-            if(flag==0) cout<<"No\n";
         }
+        return (int)f[n][m];
     }
-    return 0;
+*/
+int main(){
+    int n=10,m=100;
+    vector<vector<int>> matx(n,vector<int>(m,2));
+    cout<<matx[0][0];
 }
