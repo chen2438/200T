a8d7b199e3710944c8e5078600da0fa90fbe149d
陈昊天 <2363822647@qq.com>
2022-03-16 19:45:18 +0800

    train 3.16

--- library/tmp.cpp
+++ library/tmp.cpp
@@ -1,28 +1,17 @@
 #include<bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<=(b);++i)
 using namespace std;
-/*
-    int splitArray(vector<int>& nums, int m) {
-        int n = nums.size();
-        vector<vector<long long>> f(n + 1, vector<long long>(m + 1, LLONG_MAX));
-        //初始化二维数组f[][]，预先申请f[n+1][m+1]空间
-        vector<long long> sub(n + 1, 0);
-        for (int i = 0; i < n; i++) {
-            sub[i + 1] = sub[i] + nums[i];
-        }
-        f[0][0] = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= min(i, m); j++) {
-                for (int k = 0; k < i; k++) {
-                    f[i][j] = min(f[i][j], max(f[k][j - 1], sub[i] - sub[k]));
-                }
-            }
-        }
-        return (int)f[n][m];
-    }
-*/
-int main(){
-    int n=10,m=100;
-    vector<vector<int>> matx(n,vector<int>(m,2));
-    cout<<matx[0][0];
+
+int main() {
+    int n,k;
+  scanf("%d%d", &n, &k);
+  poly f(n + 1);
+  fact[0] = 1;
+  for (int i = 1; i <= n; ++i) fact[i] = (ll)fact[i - 1] * i % mod;
+  for (int i = 1; i <= n; ++i) f[i] = qpow(fact[i], mod - 2);
+  f = exp(log(f >> 1) * k) << k, f.resize(n + 1);
+  int inv = qpow(fact[k], mod - 2);
+  for (int i = 0; i <= n; ++i)
+    printf("%lld ", (ll)f[i] * fact[i] % mod * inv % mod);
+  return 0;
 }
