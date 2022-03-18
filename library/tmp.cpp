ff338b473a8e0a0007c1a64455f8b98f1dd0ee96
陈昊天 <2363822647@qq.com>
2022-03-18 17:59:15 +0800

    tmp

--- library/tmp.cpp
+++ library/tmp.cpp
@@ -1,30 +1,48 @@
-#include <bits/stdc++.h>
-#define ll long long
-#define mem(a) memset(a,0,sizeof(a))
+#include<bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<=(b);++i)
+#define elif else if
 using namespace std;
 
+int a[500100],c[500100],n,m;
+
+int lowbit(int x){
+    return x&-x;
+}
+
+void add(int x,int v){
+    while(x<=n)
+        c[x]+=v,x+=lowbit(x);
+}
+
+int query(int x){
+    int ans=0;
+    while(x>=1)
+        ans+=c[x],x-=lowbit(x);
+    return ans;
+}
+
 int main(){
-    ll x1,y1,x2,y2,a[25][25],h[500][500];
-    cin>>x1>>y1>>x2>>y2;
-    mem(h),mem(a);
-    x1++,x2++,y1++,y2++;
-    h[x2][y2]=1;
-    if(x2-2>=0){
-        if(y2-1>=0) h[x2-2][y2-1]=1;
-        h[x2-2][y2+1]=1;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int cmd,x,y,k;
+    cin>>n>>m;
+    int past=0,now;
+    FOR(i,1,n){
+        cin>>now;
+        add(i,now-past);
+        past=now;
+    } 
+    while(m--){
+        cin>>cmd;
+        if(cmd==1){
+            cin>>x>>y>>k;
+            add(x,k);
+            add(y+1,-k);
+        }
+        elif(cmd==2){
+            cin>>x;
+            cout<<query(x)<<'\n';
+        }
     }
-    if(x2-1>=0){
-        if(y2-2>=0) h[x2-1][y2-2]=1;
-        h[x2-1][y2+2]=1;
-    }
-    if(y2-2>=0) h[x2+1][y2-2]=1;
-    if(y2-1>=0) h[x2+2][y2-1]=1;
-    h[x2+1][y2+2]=1,h[x2+2][y2+1]=1;
-    a[1][1]=1;
-    FOR(i,1,x1)
-        FOR(j,1,y1)
-            if(!a[i][j] and !h[i][j]) a[i][j]+=a[i-1][j]+a[i][j-1];
-    cout<<a[x1][y1]<<endl;
     return 0;
 }
