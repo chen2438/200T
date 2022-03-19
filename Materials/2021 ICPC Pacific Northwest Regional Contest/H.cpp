abfd512d72ec7f4abbaa1d03c70bb3550b744e88
陈昊天 <2363822647@qq.com>
2022-03-19 22:58:08 +0800

    Update H.cpp

--- Materials/2021 ICPC Pacific Northwest Regional Contest/H.cpp
+++ Materials/2021 ICPC Pacific Northwest Regional Contest/H.cpp
@@ -2,9 +2,12 @@
 #define FOR(i,a,b) for(int i=(a);i<=(b);++i)
 using namespace std;
 
-int main(){
+int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	
-	return 0;
+   string s ;
+   cin >> s ;
+   string ss = s + s ;
+   reverse(ss.begin(), ss.end()) ;
+   cout << (ss.find(s) != string::npos) << endl ;
 }
