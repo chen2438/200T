b2a63350646f5b5126b58841596140fb5ed202fd
陈昊天 <2363822647@qq.com>
2022-03-19 21:40:11 +0800

    ICPCPacNW2022ContestData

---
+++ Materials/ICPCPacNW2022ContestData/riseandfall/submissions/accepted/rainbow_tgr.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
+using namespace std;
+
+int main(){
+   int t;
+   cin>>t;
+   while(t--){
+      string s;
+      cin>>s;
+      int i=0;
+      int lens=s.size();
+      while(i+1<lens and s[i]<=s[i+1]) ++i;
+      while(i+1<lens and s[i]>=s[i+1]) ++i;
+      while(i+1<lens) s[i+1]=s[i],++i;
+      cout<<s<<'\n';
+   }
+   return 0;
+}
