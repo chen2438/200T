45ab75d544079647d1283bde5243df8b86abc276
陈昊天 <2363822647@qq.com>
2022-03-25 00:37:24 +0800

    Update 778.C.cpp

--- codeforces/778.C.cpp
+++ codeforces/778.C.cpp
@@ -1,73 +1,29 @@
-#include<bits/stdc++.h>
-#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
-#define ll long long
-#define int long long
+#include <bits/stdc++.h>
 using namespace std;
-
-signed main(){
-    cin.tie(0)->sync_with_stdio(0);
-    int t;cin>>t;
-    while(t--){
-        multiset<int> a;
-        int n,x;
-        ll sum=0;
-        cin>>n;
-        FOR(i,1,n){
-            cin>>x;
-            sum+=x;
-            a.insert(x);
-        }
-        multiset<ll> b;
-        b.insert(sum);
-        if(n==1){
-            cout<<"YES\n";
-            continue;
-        }
-        while(!a.empty()){
-            if(*b.begin()<*a.begin()) break;
-            //cout<<"\nstFOR\n";
-
-            /*for(auto i:a)cout<<i<<" ";
-            cout<<endl;
-            for(auto i:b)cout<<i<<" ";
-            cout<<endl;*/
-
-            if(a.empty()) break;
-            auto pmax=b.end();pmax--;
-            int max0=*pmax;
-            b.erase(pmax);
-            int x=max0/2,y=max0-x;
-            auto p1=a.find(x);
-            //cout<<"max0="<<max0<<",x="<<x<<",y="<<y<<endl;
-            if(x and p1!=a.end()){
-            //    cout<<"1\n";
-                a.erase(p1);
-            }
+ 
+using ll = long long;
+ 
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t; cin >> t;
+    while (t--) {
+        ll n; cin >> n;
+        ll sum = 0;
+        vector<ll> a(n);
+        for (int i = 0; i < n; i++)
+            cin>>a[i],sum+=a[i];
+        multiset<ll> p = {sum};
+        multiset<ll> q(a.begin(), a.end());
+        while (!p.empty()){
+            ll x = *--p.end();
+            if (x < *--q.end()) break;
+            p.erase(--p.end());
+            if(q.find(x)!=q.end()) q.erase(q.find(x));
             else{
-                b.insert(x);
+                p.insert(x/2);
+                p.insert((x+1)/2);
             }
-            auto p2=a.find(y);
-            if(y and p2!=a.end()){
-            //    cout<<"2\n";
-                a.erase(p2);
-            }
-            else{
-                b.insert(y);
-            }
-            //cout<<"edFOR\n";
         }
-        /*
-        for(auto i:a)cout<<i<<" ";
-            cout<<endl;
-            for(auto i:b)cout<<i<<" ";
-            cout<<endl;
-        */
-        if(a.empty()){
-            cout<<"YES\n";
-        }
-        else{
-            cout<<"NO\n";
-        }
+        cout << (q.empty() ? "YES" : "NO") << "\n";
     }
-    return 0;
 }
