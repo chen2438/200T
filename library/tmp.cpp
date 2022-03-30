d9077fa6c66ea29443b3588d17fa9cdb0f902e22
陈昊天 <2363822647@qq.com>
2022-03-30 20:12:58 +0800

    tmp

--- library/tmp.cpp
+++ library/tmp.cpp
@@ -1,34 +1,35 @@
-#include<bits/stdc++.h>
+#include <iostream>
 #define FOR(i,a,b) for(int i=(a);i<=(b);++i)
 using namespace std;
-const int maxn=5e5+7; 
 
-int a[maxn];
+const int MAXN = 1007;
+
+int n, a[MAXN],dp[MAXN],from[MAXN];
+
+void output(int x){
+    if(!x) return;
+    output(from[x]);
+    cout<<a[x]<<" ";
+}
 
 int main(){
-    int n;
-    cin>>n;
-
-    for(int i=1;i<=n;i++){
-        a[i]=n%i;
-        cout<<i<<" "<<a[i]<<endl;
-    }
-    cout<<"\n\n";
-    cout<<1<<" "<<a[1]<<" --- ";
-    for(int i=2;i<=n;i++){
-        if(a[i-1]<a[i]){
-            cout<<i-1<<" "<<a[i-1]<<endl;
-            cout<<i<<" "<<a[i]<<" --- ";
+    n=1;
+    while(cin>>a[n]) ++n;
+    n--;
+    FOR(i,1,n){
+        dp[i]=1;
+        from[i]=0;
+        FOR(j,1,i-1){
+            if(a[j]<a[i] and dp[i]<dp[j]+1)
+                dp[i]=dp[j]+1,from[i]=j;
         }
     }
-    cout<<"\n\n";
-    set<int> s;
-    for(int i=n;i>=1;i--){
-        s.insert(n/i);
+    
+    int ans=dp[1],pos=1;
+    FOR(i,1,n){
+        if(ans<dp[i]) ans=dp[i],pos=i;
     }
-    for(auto i:s){
-        cout<<i<<" ";
-    }
-   
+    cout<<"max="<<ans<<endl;
+    output(pos);
     return 0;
 }
