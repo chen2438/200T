07e8b216cac9655141d0b6bd3e39812fc72459a3
陈昊天 <2363822647@qq.com>
2022-04-20 15:42:17 +0800

    clear library

--- library/tmp.cpp
+++ library/tmp.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 signed main(){
     cin.tie(0)->sync_with_stdio(0);
-    string s1(5,'a');
-    cout<<s1;
+    vector<int> a={1,2,3,4,5};
+    vector<int> sum(a.size(),0);//sum.resize(a.size());
+    partial_sum(a.begin(),a.end(),sum.begin(),[](int _x,int _y){return _x-_y;});
+    for(auto i:sum)cout<<i<<" ";
     return 0;
 }
