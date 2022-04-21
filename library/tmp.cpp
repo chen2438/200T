bead27f28e8d527feb60d3a005efb6065927b53d
陈昊天 <2363822647@qq.com>
2022-04-21 17:47:35 +0800

    tmp

--- library/tmp.cpp
+++ library/tmp.cpp
@@ -2,11 +2,16 @@
 #define FOR(i,a,b) for(int i=(a);i<=(b);++i)
 using namespace std;
 
+multiset<string> ms;
+
 signed main(){
     cin.tie(0)->sync_with_stdio(0);
-    vector<int> a={1,2,3,4,5};
-    vector<int> sum(a.size(),0);//sum.resize(a.size());
-    partial_sum(a.begin(),a.end(),sum.begin(),[](int _x,int _y){return _x-_y;});
-    for(auto i:sum)cout<<i<<" ";
+    int n;cin>>n;
+    string str;
+    FOR(i,1,n){
+        cin>>str;
+        cout<<ms.count(str)<<'\n';
+        ms.insert(str);
+    }
     return 0;
 }
