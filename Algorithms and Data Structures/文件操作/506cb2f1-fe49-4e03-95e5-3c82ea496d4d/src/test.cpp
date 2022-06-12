82f80dbedef4e1dfd25ff3e807f6bd86f64b781d
陈昊天 <2363822647@qq.com>
2022-06-12 16:28:30 +0800

    up

--- Algorithms and Data Structures/文件操作/506cb2f1-fe49-4e03-95e5-3c82ea496d4d/src/test.cpp
+++ Algorithms and Data Structures/文件操作/506cb2f1-fe49-4e03-95e5-3c82ea496d4d/src/test.cpp
@@ -1,35 +1,20 @@
 //#include<bits/stdc++.h>
-#include<stdio.h>
+/*#include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-#include<string.h>
+#include<string.h>*/
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+#include<ctime>
+#include<unistd.h>
 #define FOR(i,a,b) for(int i=(a);i<=(b);++i)
-//using namespace std;
+using namespace std;
 
-int prime(int n){
-	//if(n)
-	FOR(i,2,sqrt(n)){
-		if(n%i==0) return 1;
-	}
-	return 0;
-}
-
-signed main(){
-	//cin.tie(0)->sync_with_stdio(0);
-	int n=2,cnt=0;
-	FILE *fp=fopen("in.txt","w");
-	/*if((fp=fopen("in.txt","w"))==NULL){
-		puts("open error");exit(0);
-	}*/
-	while(cnt<500){
-		if(prime(n)){
-			cnt++;
-			fprintf(fp,"%d ",n);
-		}
-		n++;
-	}
-	if(fclose(fp)){
-		puts("close error");exit(0);
-	}
+int main(){
+	int a=10;
+	//cin>>a;
+	cout<<a+10;
+	puts("debug1");
 	return 0;
 }
