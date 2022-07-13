109ab6a1c8ca683a573e454610dd062808af5be6
陈昊天 <2363822647@qq.com>
2022-07-13 16:38:05 +0800

    Update basic.cpp

--- 模板/basic.cpp
+++ 模板/basic.cpp
@@ -19,7 +19,10 @@ using namespace std;
 
 signed main(){
 	cin.tie(0)->sync_with_stdio(0);
-	
+	int T;cin>>T;
+    while(T--){
+        solve();
+    }
 	return 0;
 }
 /*----------------------------------------*/
