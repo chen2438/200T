676f7c3a6b37dcad136e37583c66fa467cdc508e
陈昊天 <2363822647@qq.com>
2022-07-15 16:43:39 +0800

    Update basic.cpp

--- 模板/basic.cpp
+++ 模板/basic.cpp
@@ -17,13 +17,13 @@ using ll=long long;
 #define FOR(i,a,b) for(int i=(a);i<=(b);++i)
 using namespace std;
 
+void solve(){}
+
 signed main(){
-	cin.tie(0)->sync_with_stdio(0);
-	int T;cin>>T;
-    while(T--){
-        solve();
-    }
-	return 0;
+    cin.tie(0)->sync_with_stdio(0);
+    int T; cin>>T;
+    while(T--) solve();
+    return 0;
 }
 /*----------------------------------------*/
 #define ROF(i,a,b) for(int i=(a);i>=(b);--i)
