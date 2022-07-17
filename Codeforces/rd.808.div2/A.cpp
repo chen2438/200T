b25d109095a091cc8360dec666066376a0741dea
chen2438 <2363822647@qq.com>
2022-07-17 12:02:44 +0800

    Create A.cpp

---
+++ Codeforces/rd.808.div2/A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
+using namespace std;
+
+void solve(){}
+
+signed main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int T; cin>>T;
+    while(T--) solve();
+    return 0;
+}
