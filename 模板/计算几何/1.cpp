b5eee3dd4cbdcdd52e5d141f00fa1a7cc6c90666
陈昊天 <2363822647@qq.com>
2022-08-02 16:50:29 +0800

    geo

--- 模板/计算几何/1.cpp
+++ 模板/计算几何/1.cpp
@@ -1,3 +1,13 @@
+#pragma GCC optimize(3)
+#include<bits/stdc++.h>
+#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
+#define ROF(i,a,b) for(int i=(a);i>=(b);--i)
+#define mem(a) memset((a),0,sizeof(a))
+#define endl '\n'
+// #define int long long
+#define double long double
+using namespace std;
+
 namespace geometry{
     const double PI = acos(-1);
     const double eps = 1e-8;
@@ -6,7 +16,7 @@ namespace geometry{
         if(x<0) return -1;
         return 1;
     }
-    struct PT{
+    struct PT {
         double x,y;
         PT() {x=0,y=0;}
         PT(double a,double b) {x=a,y=b;}
@@ -31,36 +41,12 @@ namespace geometry{
             return {nx,ny};}
         double get_angle(){return atan2(y,x);}
     };
- 
+
     struct LI{
         PT st, ed;//起点，终点
         double get_angle()const{return atan2(ed.y-st.y,ed.x-st.x);}
     };
-     
-    struct CI{
-        double x,y,r;
-        CI() {x=0,y=0,r=0;}
-        CI(double a,double b,double c) {x=a,y=b,r=c;}
-        friend ostream &operator<<(ostream &o,const CI &X){
-            o<<X.x<<' '<<X.y<<' '<<X.r;return o;}
-        friend istream &operator>>(istream &i,CI &X){ 
-            i>>X.x>>X.y>>X.r;return i;}
-        CI operator-() {return CI(-x,-y,-r);}
-        CI operator+(const CI &B){return {x+B.x,y+B.y,r+B.r};}
-        CI operator-(const CI &B){return {x-B.x,y-B.y,r-B.r};}
-        CI operator*(const double k){return {x*k,y*k,r*k};}
-        friend CI operator*(const double k,CI T){return T*k;}
-        bool operator==(const CI &Y){
-            return (!sign(x-Y.x))&&(!sign(y-Y.y))&&(!sign(r-Y.r));}
-        bool operator!=(const CI &Y){
-            return sign(x-Y.x)||sign(y-Y.y)||sign(r-Y.r);}
-        double area(){return PI*r*r;}
-        double girth(){return 2*PI*r;}
-    };
-     
     namespace geofunc{
-    double len(PT a,PT b){return (a-b).size();}
-    double cdis(CI a,CI b){return len({a.x,a.y},{b.x,b.y});}
     double dot(PT a, PT b) {return a.x*b.x+a.y*b.y;}
     double cross(PT a,PT b){return a.x*b.y-b.x*a.y;}
     //B在A的逆时针方向为正
@@ -84,10 +70,6 @@ namespace geometry{
         return sqrt(p*(p-a)*(p-b)*(p-c));
     }
     double area(PT OA,PT OB){return cross(OA,OB)/2;}
-    double cos_law(double a,double b,double c){
-        double cosC=(a*a+b*b-c*c)/(2*a*b);
-        return acos(cosC);
-    }
     PT line_x_line(PT p,PT v,PT q,PT w){
         //以p为起点,q为方向向量的直线l1与l2的交点
         PT u = p - q;
@@ -147,5 +129,109 @@ namespace geometry{
     }
     using namespace geofunc;
 }
- 
+
 using namespace geometry;
+
+const int N = 1007;
+
+int n;
+double r[N],x[N],y[N];
+struct SEC{
+    double l,r;
+    int nojoin;
+    bool operator<(const SEC &y)const{
+        return l<y.l;
+    }
+};
+
+vector<int> cid;
+vector<SEC> sec[N];
+vector<SEC> uni[N];
+
+double cos_law(double a,double b,double c){
+    double cosC=(a*a+b*b-c*c)/(2*a*b);
+    return acos(cosC);
+}
+
+SEC get_cover(int last,int now){//返回旧圆被覆盖的弧度
+    PT clast={x[last],y[last]};
+    PT cnow={x[now],y[now]};
+    double cdist=(clast-cnow).size();
+    if(sign(cdist-(r[last]+r[now]))>=0)//两圆分离
+        return {0,0};
+    if(sign(r[now]-(cdist+r[last]))>=0)//新圆覆盖旧圆
+        return {-PI,PI};
+    if(sign(r[last]-(cdist+r[now]))>=0)//旧圆覆盖新圆
+        return {0,0,1};//debug!!!
+    LI O1O2={clast,cnow};
+    double angle=O1O2.get_angle();
+    double theta=cos_law(r[last],cdist,r[now]);
+    SEC ans={angle-theta,angle+theta};
+    return ans;
+}
+
+void solve(){
+    cin>>n;
+    FOR(i,1,n){//注意输入顺序
+        cin>>r[i]>>x[i]>>y[i];
+    }
+    FOR(i,1,n){
+        int join=1;
+        for(auto j:cid){
+            SEC T=get_cover(j,i);
+            // cout<<ang(T.l)<<" "<<ang(T.r)<<endl;
+            if(T.nojoin) join=0;
+            else{
+                if(T.r>PI){
+                    sec[j].push_back({T.l,PI});
+                    sec[j].push_back({-PI,T.r-2*PI});
+                }
+                else sec[j].push_back(T);
+            }
+        }
+        if(join) cid.push_back(i);
+    }
+    // cout<<"cid\n";
+    // for(auto i:cid) cout<<i<<" ";cout<<endl;
+    // cout<<"-----------\n";
+    // for(auto i:cid){
+    //     for(auto j:sec[i]){
+    //         cout<<j.l<<" "<<j.r<<endl;
+    //     }
+    // }
+    double ans=0;
+    for(auto i:cid){
+        double idiv=0;
+        if(sec[i].size()==0){
+            ans+=2*PI*r[i];
+            continue;
+        }
+        if(sec[i].size()==1){
+            idiv=sec[i][0].r-sec[i][0].l;
+            ans+=(2*PI-idiv)*r[i];
+            continue;
+        }
+        sort(sec[i].begin(),sec[i].end());
+        uni[i].push_back(sec[i][0]);
+        double lastr=uni[i][0].r;
+        FOR(j,1,sec[i].size()-1){
+            if(lastr>sec[i][j].l){
+                lastr=sec[i][j].r;
+                uni[i][uni[i].size()-1].r=lastr;
+            }
+            else uni[i].push_back(sec[i][j]);
+        }
+        for(auto j:uni[i]){
+            idiv+=j.r-j.l;
+        }
+        ans+=(2*PI-idiv)*r[i];
+    }
+    cout<<ans<<endl;
+}
+
+signed main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int T=1; //cin>>T;
+    while(T--) solve();
+    return 0;
+}
